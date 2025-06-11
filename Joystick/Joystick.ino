1b1d35bf5dcf7c1fe351c8997fd770a017178a27
Rohan <rohan.kamath.24@gmail.com>
2025-06-11 21:19:30 +0530

    Added sketch

--- Joystick/Joystick.ino
+++ Joystick/Joystick.ino
@@ -4,6 +4,7 @@ int servoPin = 9;
 int xPin = A0;  
 int yPin = A1;  
 int sPin = 2;   
+int buzzPin = 13;
 
 int xVal, yVal, sVal;
 int servoAngle;
@@ -16,15 +17,18 @@ void setup() {
   pinMode(xPin, INPUT);
   pinMode(yPin, INPUT);
   pinMode(sPin, INPUT);
+  pinMode(buzzPin, OUTPUT);
   digitalWrite(sPin, HIGH); 
   myServo.attach(servoPin);
 }
 
 void loop() {
+  digitalWrite(buzzPin, LOW);
   xVal = analogRead(xPin); 
   yVal = analogRead(yPin);  
   servoAngle = map(yVal, 1023, 0, 0, 180);
   servoAngle = constrain(servoAngle, 0, 180);
+  if(servoAngle==180) ringBuzzer();
   static int prevAngle = -1;
   if (abs(servoAngle - prevAngle) >= 2) {
     myServo.write(servoAngle);
@@ -33,4 +37,8 @@ void loop() {
   Serial.print("Servo angle = ");
   Serial.println(servoAngle);
   delay(delayTime);
+}
+
+void ringBuzzer(){
+  digitalWrite(buzzPin, HIGH);
 }
